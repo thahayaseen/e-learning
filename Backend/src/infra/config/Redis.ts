6075e51fe5a4195722448dca3f0d5b27669d0a08
thahayaseen <kthahayaseen@gmail.com>
2025-04-09 21:05:58 +0530

    added start

--- Backend/src/infra/config/Redis.ts
+++ Backend/src/infra/config/Redis.ts
@@ -1,7 +1,7 @@
 import { createClient } from "redis";
 
 // Check for environment variables for Redis connection, if not set, fallback to default
-const redisHost = process.env.REDIS_HOST || "redis://localhost"; // Default to 'localhost' if not set
+const redisHost = process.env.REDIS_HOST || "localhost"; // Default to 'localhost' if not set
 const redisPort = Number(process.env.REDIS_PORT) || 6379; // Default to 6379 if not set
 console.log("redis conecting ", redisHost, redisPort);
 const redisUrl = `redis://${redisHost}:${redisPort}`;
