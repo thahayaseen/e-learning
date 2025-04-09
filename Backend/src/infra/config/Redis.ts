6a0e490cdd068cdb182f3cae9f8112c1aac020fd
thahayaseen <kthahayaseen@gmail.com>
2025-04-09 17:46:03 +0530

    fixed redis config

--- Backend/src/infra/config/Redis.ts
+++ Backend/src/infra/config/Redis.ts
@@ -4,9 +4,11 @@ import { createClient } from "redis";
 const redisHost = process.env.REDIS_HOST || "redis://localhost"; // Default to 'localhost' if not set
 const redisPort = Number(process.env.REDIS_PORT) || 6379; // Default to 6379 if not set
 console.log("redis conecting ", redisHost, redisPort);
+const redisUrl = `redis://${redisHost}:${redisPort}`;
+console.log(redisUrl,'url ias ');
 
 const redis = createClient({
-  url: redisHost + ":" + redisPort,
+  url: redisUrl,
 });
 
 export default redis;
