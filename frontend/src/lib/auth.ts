12a0e58e4f24efb32bec27a2cda97665a501b334
thahayaseen <kthahayaseen@gmail.com>
2025-04-10 13:03:45 +0530

    chabnge the local host

--- frontend/src/lib/auth.ts
+++ frontend/src/lib/auth.ts
@@ -6,7 +6,7 @@ export const clearGs = async (dispatch: AppDispatch) => {
   console.log("in here");
   dispatch(logout());
   await axios.post(
-    `http://${process.env.NEXT_PUBLIC_DOMAIN}:4050/logout`,
+    `https://${process.env.NEXT_PUBLIC_DOMAIN}:4050/logout`,
     {},
     { withCredentials: true }
   );
