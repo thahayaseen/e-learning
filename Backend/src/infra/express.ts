5b640f0473a5917d8fd8cd3c105e768d64a639e8
thahayaseen <kthahayaseen@gmail.com>
2025-04-10 08:51:07 +0530

    added letest domain

--- Backend/src/infra/express.ts
+++ Backend/src/infra/express.ts
@@ -12,12 +12,18 @@ app.use(morgan("dev"))
 app.use(express.static(path.join(__dirname,'public')))
 app.use(
   cores({
-    origin: ["http://172.16.1.135:3000","http://localhost:3000","e-learning-8155s9qs0-thahayaseens-projects.vercel.app"],
+    origin: [
+      "http://172.16.1.135:3000",
+      "http://localhost:3000",
+      "https://e-learning-8155s9qs0-thahayaseens-projects.vercel.app",
+      process.env.NEXT_PUBLIC_SERVER!,
+    ],
     methods: ["GET", "POST", "PATCH", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
   })
 );
+
 app.use(express.json());
 app.use(cookieparser());
 app.use('/',router)
